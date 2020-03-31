ad8707c3b41060af7345219f2b45b97007fcd683
Luís <lfilipems@hotmail.com>
2020-03-31 17:37:20 +0100

    Implementing Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,5 +26,17 @@ namespace SalesWebMvc.Services
             _context.Seller.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id) 
+        {
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = FindById(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
     }
 }
