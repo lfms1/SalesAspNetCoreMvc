fa2c786372a93773e39f045a1fa7d343220acc4c
Luís <lfilipems@hotmail.com>
2020-03-31 15:53:16 +0100

    Implementing Create Seller Form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller) 
+        {
+            _context.Seller.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
