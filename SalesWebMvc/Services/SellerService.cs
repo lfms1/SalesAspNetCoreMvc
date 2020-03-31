616d77ce1ed669c45b328dacf19959bb560a7202
Luís <lfilipems@hotmail.com>
2020-03-31 16:05:00 +0100

    Department FK

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller seller) 
         {
+            seller.Department = _context.Department.First();
             _context.Seller.Add(seller);
             _context.SaveChanges();
         }
